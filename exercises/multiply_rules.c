9e38e5b477fdb8d4a01ccbfc17fce8d716878ff5
boblee200508 <bob_lee200508@163.com>
2018-01-18 10:54:42 +0800

    Finish!

---
+++ exercises/multiply_rules.c
@@ -0,0 +1,15 @@
+/*
+    在屏幕上打印乘法口诀表
+*/
+
+#include <stdio.h>
+
+int main() {
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= i; j++) {
+			printf("%dx%d=%-2d ", j, i, i * j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
